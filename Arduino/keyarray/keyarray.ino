27a000ad8ca31ed3b9592bee24e656a5d9a990fa
TaeaT <spgstym@ajou.ac.kr>
2016-12-01 15:12:35 +0900

    change to membrane keyboard

--- Arduino/keyarray/keyarray.ino
+++ Arduino/keyarray/keyarray.ino
@@ -17,6 +17,21 @@ char keys[rows][cols] = {
 byte rowPins[rows] = {11, 10, 9, 8};
 byte colPins[cols] = {7, 6, 5, 4};
 
+String btn1 = "1";
+String btn2 = "2";
+String btn3 = "3";
+String btn4 = "4";
+String btn5 = "5";
+String btn6 = "6";
+String btn7 = "7";
+String btn8 = "8";
+String btn9 = "9";
+String btn10 = "10";
+String btn11 = "11";
+String btn12 = "12";
+String btn13 = "13";
+String btn14 = "14";
+
 Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
 
 void setup(){
@@ -34,7 +49,7 @@ void loop(){
 
       case '1':
       Serial.println("1");
-      BTSerial.println("1");
+      BTSerial.println(btn1);
       break;
 
       case '2':
@@ -48,48 +63,48 @@ void loop(){
       break;
 
       case 'A':
-      Serial.println("A");
-      BTSerial.println("A");
-      break;
-
-      case '4':
       Serial.println("4");
       BTSerial.println("4");
       break;
 
-      case '5':
+      case '4':
       Serial.println("5");
       BTSerial.println("5");
       break;
 
-      case '6':
+      case '5':
       Serial.println("6");
       BTSerial.println("6");
       break;
 
-      case 'B':
-      Serial.println("B");
-      BTSerial.println("B");
-      break;
-
-      case '7':
+      case '6':
       Serial.println("7");
       BTSerial.println("7");
       break;
 
-      case '8':
+      case 'B':
       Serial.println("8");
       BTSerial.println("8");
       break;
 
-      case '9':
+      case '7':
       Serial.println("9");
       BTSerial.println("9");
       break;
 
+      case '8':
+      Serial.println("10");
+      BTSerial.println("10");
+      break;
+
+      case '9':
+      Serial.println("11");
+      BTSerial.println("11");
+      break;
+
       case 'C':
-      Serial.println("C");
-      BTSerial.println("C");
+      Serial.println("12");
+      BTSerial.println("12");
       break;
 
       case '*':
@@ -98,13 +113,13 @@ void loop(){
       break;
 
       case '0':
-      Serial.println("0");
-      BTSerial.println("0");
+      Serial.println("13");
+      BTSerial.println("13");
       break;
 
       case '#':
-      Serial.println("#");
-      BTSerial.println("#");
+      Serial.println("14");
+      BTSerial.println("14");
       break;
 
       case 'D':
